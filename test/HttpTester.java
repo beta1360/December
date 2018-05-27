89f039815c0cba8caf57b6cafa852f8f2cc2c4c5
K.H Lee <beta1360@naver.com>
2018-05-28 00:29:06 +0900

    fix#6 update test case

--- test/HttpTester.java
+++ test/HttpTester.java
@@ -1,10 +1,35 @@
 package test;
+import December.HttpRequest;
+import December.HttpResponse;
+import December.RouteTask;
 import December.Server;
+import org.json.simple.JSONObject;
 
 import java.io.IOException;
 
 public class HttpTester {
     public static void main(String[] args) throws IOException {
-        new Server(8000).run();
+        Server server = new Server(8000);
+
+        server.route("/", "GET", new RouteTask() {
+            @Override
+            public String task(HttpRequest request, HttpResponse response) {
+                JSONObject json = new JSONObject();
+                json.put("hello", "world!");
+                String res = response.jsonify(json);
+                return res;
+            }
+        });
+
+        server.route("/home", "GET", new RouteTask() {
+            @Override
+            public String task(HttpRequest request, HttpResponse response) {
+                JSONObject json = new JSONObject();
+                json.put("hello", "world! get!");
+                String res = response.jsonify(json);
+                return res;
+            }
+        });
+        server.run();
     }
 }
