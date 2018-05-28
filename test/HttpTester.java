d289737dd4882df4af6d3c3e42e0d88290cfe71a
K.H Lee <beta1360@naver.com>
2018-05-29 07:16:15 +0900

    fix#10 add and modify test case

--- test/HttpTester.java
+++ test/HttpTester.java
@@ -4,32 +4,45 @@ import December.HttpResponse;
 import December.RouteTask;
 import December.Server;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 
 public class HttpTester {
     public static void main(String[] args) throws IOException {
+
+
         Server server = new Server(8000);
+        server.setStaticPath("C:\\Users\\user\\IdeaProjects\\MyWebFramework\\src\\test\\");
 
-        server.route("/", "GET", new RouteTask() {
+        server.route("/image1.jpg", "GET", new RouteTask() {
             @Override
             public String task(HttpRequest request, HttpResponse response) {
-                JSONObject json = new JSONObject();
-                json.put("hello", "world!");
-                String res = response.jsonify(json);
-                return res;
+                return null;
             }
         });
 
-        server.route("/home", "GET", new RouteTask() {
+        server.route("/message", "POST", new RouteTask() {
             @Override
             public String task(HttpRequest request, HttpResponse response) {
-                JSONObject json = new JSONObject();
-                json.put("hello", "world! get!");
-                String res = response.jsonify(json);
-                return res;
+                JSONObject req = request.json();
+                String message = (String)req.get("content");
+
+                String res_msg ;
+                if(message.equals("hello"))
+                    res_msg = "hello:)";
+                else res_msg = "hungry :(";
+
+                JSONObject res = new JSONObject();
+                JSONObject temp = new JSONObject();
+                temp.put("text", res_msg);
+                res.put("message", temp);
+
+                return response.jsonify(res);
             }
         });
+
         server.run();
     }
 }
